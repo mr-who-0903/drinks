83eda688fd552a661489276f69ec59f887254d64
unknown <ritikjain0903@gmail.com>
2020-12-06 17:02:28 +0530

    Updates

---
+++ precache-manifest.646d936b961dbe55550210de1cfb7677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d1152109ff2fa4134f9ac1bfa720ca",
+    "url": "/drinks/index.html"
+  },
+  {
+    "revision": "75a6a17679dba8ee022f",
+    "url": "/drinks/static/css/main.f14390d6.chunk.css"
+  },
+  {
+    "revision": "c7c66b13b38631aa7d50",
+    "url": "/drinks/static/js/2.a09379b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drinks/static/js/2.a09379b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a6a17679dba8ee022f",
+    "url": "/drinks/static/js/main.27f1fbf2.chunk.js"
+  },
+  {
+    "revision": "2fef2d76d9ec0e5a13c3",
+    "url": "/drinks/static/js/runtime-main.1984347e.js"
+  },
+  {
+    "revision": "d3c4ed9a7742284fbd12a840227266ed",
+    "url": "/drinks/static/media/bg.d3c4ed9a.jpg"
+  }
+]);
